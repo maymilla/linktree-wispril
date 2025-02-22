9cb90f562b7cf10286cf59bc0970e6dd3634510c
Mayla Yaffa <maylayaffa12@gmail.com>
2025-02-22 11:24:10 +0700

    fix: bg color

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import LinkButton from "./button";
 export default function Home() {
 
   return (
-    <div className="shadow-inner bg-gradient-to-tr fgitrom-rose-800 to-amber-200 p-8 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="shadow-inner bg-gradient-to-tr from-rose-800 to-amber-200 p-8 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="text-center sm:text-left">
           <h1 className="text-white text-3xl sm:text-4xl font-bold mb-6">Daftar Website Wisuda April ITB</h1>

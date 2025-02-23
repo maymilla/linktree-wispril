126b8e1658768ae6687a5063010aca32bd0064fb
Mayla Yaffa <maylayaffa12@gmail.com>
2025-02-23 08:00:18 +0700

    fix: text

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       </main>
 
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <p className="text-white">Made by Mayla Yaffa</p>
+        <p className="text-white">Parade Wisuda ITB</p>
       </footer>
     </div>
   );
